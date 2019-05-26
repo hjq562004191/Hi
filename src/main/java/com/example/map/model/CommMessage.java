63ee38dbe67a90edb97680fe0c09794749c3f7a4
hjq562004191 <562004191@qq.com>
2019-05-26 11:26:14 +0800

    将评论封装成对象

---
+++ src/main/java/com/example/map/model/CommMessage.java
@@ -0,0 +1,17 @@
+package com.example.map.model;
+
+/**
+ * @author Qiang
+ * @data 2019/5/26/11:17
+ */
+public class CommMessage {
+    private String comm;
+
+    public String getComm() {
+        return comm;
+    }
+
+    public void setComm(String comm) {
+        this.comm = comm;
+    }
+}
